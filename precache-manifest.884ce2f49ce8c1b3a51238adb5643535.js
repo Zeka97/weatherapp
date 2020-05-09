fe2c811d3bb8ce08c8ecbb5bb039c79b437ce078
Amel <zekafrombtm@hotmail.com>
2020-05-10 00:33:48 +0200

    Updates

---
+++ precache-manifest.884ce2f49ce8c1b3a51238adb5643535.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8be9dbc040f6eb8f9f7c425c52a5e607",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "0a2396ea14f003aeb8e0",
+    "url": "/weatherapp/static/css/main.a8006175.chunk.css"
+  },
+  {
+    "revision": "607fa5c8c73784c46024",
+    "url": "/weatherapp/static/js/2.095e9669.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weatherapp/static/js/2.095e9669.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a2396ea14f003aeb8e0",
+    "url": "/weatherapp/static/js/main.02655d62.chunk.js"
+  },
+  {
+    "revision": "37a461f81c69d6959f2d",
+    "url": "/weatherapp/static/js/runtime-main.5392d643.js"
+  }
+]);
